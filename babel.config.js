72d45ef4cf277e6e73047f07040b8650be7c0a0e
Ryan Johnson <ryan@sprucelabs.ai>
2020-11-23 11:52:45 -0500

    chore: fix babel

--- babel.config.js
+++ babel.config.js
@@ -5,6 +5,18 @@ module.exports = (api) => {
 		ignore: ["**/testDirsAndFiles/**", "**/spruce-templates/src/templates/**"],
 		presets: ['@babel/preset-env', '@babel/preset-typescript'],
 		plugins: [
+			['@sprucelabs/babel-plugin-schema', {
+				cwd: __dirname,
+				destination: process.env.PWD
+			}],
+			'@babel/plugin-transform-runtime',
+			[
+				'@babel/plugin-proposal-decorators',
+				{
+					legacy: true,
+				},
+			],
+			['@babel/plugin-proposal-class-properties', {loose: true}],
 			['module-resolver',
 			{
 				root: ['./'],
