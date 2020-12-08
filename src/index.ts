60c5d210604446551692a81ca7c6e044401ae9c0
Taylor Romero <taylorromero@taysmacbookpro.local>
2020-12-07 18:51:14 -0700

    minor: all schemas output

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,12 @@
+export { default as defaultsByRoleSchema } from './.spruce/schemas/mercuryTypes/v2020_09_01/defaultsByRole.schema'
 export { default as eventContractSchema } from './.spruce/schemas/mercuryTypes/v2020_09_01/eventContract.schema'
+export { default as eventSignatureSchema } from './.spruce/schemas/mercuryTypes/v2020_09_01/eventSignature.schema'
+export { default as eventSignatureByNameSchema } from './.spruce/schemas/mercuryTypes/v2020_09_01/eventSignaturesByName.schema'
+export { default as permissionSchema } from './.spruce/schemas/mercuryTypes/v2020_09_01/permission.schema'
 export { default as permissionContractSchema } from './.spruce/schemas/mercuryTypes/v2020_09_01/permissionContract.schema'
+export { default as statusFlagsSchema } from './.spruce/schemas/mercuryTypes/v2020_09_01/statusFlags.schema'
+
+
 export * from './mercury.types'
 export { default as MercuryEventEmitter } from './mercury.types'
 export * from './constants'
