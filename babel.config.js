2e7188f0addd7879469accc3760582dab4109f8c
Ryan Johnson <ryan@sprucelabs.ai>
2021-02-25 12:36:03 -0500

    chore: add esm to package

--- babel.config.js
+++ babel.config.js
@@ -2,13 +2,16 @@ module.exports = (api) => {
 	api.cache(false)
 
 	return {
-		ignore: ["**/testDirsAndFiles/**", "**/spruce-templates/src/templates/**"],
+		ignore: ['**/testDirsAndFiles/**', '**/spruce-templates/src/templates/**'],
 		presets: ['@babel/preset-env', '@babel/preset-typescript'],
 		plugins: [
-			['@sprucelabs/babel-plugin-schema', {
-				cwd: __dirname,
-				destination: process.env.PWD
-			}],
+			[
+				'@sprucelabs/babel-plugin-schema',
+				{
+					cwd: __dirname,
+					destination: process.env.PWD,
+				},
+			],
 			'@babel/plugin-transform-runtime',
 			[
 				'@babel/plugin-proposal-decorators',
@@ -16,14 +19,16 @@ module.exports = (api) => {
 					legacy: true,
 				},
 			],
-			['@babel/plugin-proposal-class-properties', {loose: true}],
-			['module-resolver',
-			{
-				root: ['./'],
-				alias: {
-					'#spruce': './src/.spruce',
+			['@babel/plugin-proposal-class-properties', { loose: true }],
+			[
+				'module-resolver',
+				{
+					root: ['./'],
+					alias: {
+						'#spruce': './src/.spruce',
+					},
 				},
-			}],
-		]
+			],
+		],
 	}
 }
