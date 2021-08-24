b6f10d1f7a29b476537471067876783268b1f60d
Tay <me@taylorrome.ro>
2021-08-24 13:53:29 -0600

    patch: dependency updates

--- src/.spruce/errors/options.types.ts
+++ src/.spruce/errors/options.types.ts
@@ -1,11 +1,10 @@
 import { SpruceErrors } from "#spruce/errors/errors.types"
-import { SpruceErrorOptions, ErrorOptions as ISpruceErrorOptions} from "@sprucelabs/error"
-import { SchemaErrorOptions } from '@sprucelabs/schema'
+import { ErrorOptions as ISpruceErrorOptions} from "@sprucelabs/error"
 
 export interface InvalidEventContractErrorOptions extends SpruceErrors.Mercury.InvalidEventContract, ISpruceErrorOptions {
 	code: 'INVALID_EVENT_CONTRACT'
 }
 
-type ErrorOptions = SchemaErrorOptions | SpruceErrorOptions | InvalidEventContractErrorOptions 
+type ErrorOptions =  | InvalidEventContractErrorOptions 
 
 export default ErrorOptions
