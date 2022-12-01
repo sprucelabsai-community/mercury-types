0493941929fb7ee72872d9c27b877b361a20444e
Taylor Romero <me@taylorrome.ro>
2022-11-30 19:14:00 -0700

    minor: export name fix

--- src/index.ts
+++ src/index.ts
@@ -14,5 +14,9 @@ export { default as MercuryEventEmitter } from './types/mercury.types'
 export * from './constants'
 export { default as buildEventContract } from './utilities/buildEventContract'
 export { default as buildPermissionContract } from './utilities/buildPermissionContract'
+/**
+ * @depracted buildPermissionReferenc -> buildPermissionReference
+ */
 export { default as buildPermissionReferenc } from './utilities/buildPermissionReference'
+export { default as buildPermissionReference } from './utilities/buildPermissionReference'
 export { default as validateEventContract } from './utilities/validateEventContract'
